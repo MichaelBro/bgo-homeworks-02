05e01c4e58fecd4bdd90cce22a718b52b34564f7
MichaelBro <michael.bro@mail.ru>
2020-12-20 13:38:29 +0300

    added round for ratePerMonthPercent and for monthlyPayment

--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -3,12 +3,12 @@ package credit
 import "math"
 
 func Calculate(amountCredit, periodCredit, percentPerYearCredit int) (monthlyPayment, overPayment, totalPayment int) {
-	ratePerMonthPercent := float64(percentPerYearCredit) / 12 / 100
+	ratePerMonthPercent := math.Round((float64(percentPerYearCredit)/12/100)*1000) / 1000
 
 	exp := math.Pow(1+ratePerMonthPercent, float64(periodCredit))
 	coefficient := (ratePerMonthPercent * exp) / (exp - 1)
 
-	monthlyPayment = int(coefficient * float64(amountCredit))
+	monthlyPayment = int(math.Round(coefficient*1_000_000)) * amountCredit / 1_000_000
 	totalPayment = monthlyPayment * periodCredit
 	overPayment = totalPayment - amountCredit
 
